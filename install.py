3a6a9e70e367c07000236a74f2f0512d49fb3239
Micah Ferrill <mcferrill@gmail.com>
2013-01-04 15:23:56 -0600

    PEP8 fixes

--- install.py
+++ install.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
+from os.path import dirname, abspath, basename, join, splitext
 import glob
 import sys
 import os
 
+
 def main():
 
     # Move to the directory above the script's location (home folder)
-    os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    os.chdir(dirname(dirname(abspath(__file__))))
 
     # Rename our directory to .files
-    os.rename(os.path.basename(os.path.dirname(__file__)), '.files')
+    os.rename(basename(dirname(__file__)), '.files')
 
     # Create a "junction" (symlink-like) to home/bin.
     if sys.platform in ('win32', 'cygwin'):
-       os.system(r'.files/bin/junction.exe bin .files/bin')
+        os.system(r'.files/bin/junction.exe bin .files/bin')
 
     # Windows
     if sys.platform == 'win23':
         # Add home/bin to our system path.
-        os.system('SET PATH=%%PATH%%;%s' % os.path.join(os.path.abspath(os.curdir)))
+        os.system('SET PATH=%%PATH%%;%s' % join(abspath(os.curdir)))
 
         # Add the .py extension to the system pathext variable so we can call
         # python scripts directly.
@@ -29,12 +31,15 @@ def main():
     else:
 
         # Make sure home/bin exists.
-        if not os.path.exists('bin'): os.makedirs('bin')
+        if not os.path.exists('bin'):
+            os.makedirs('bin')
 
         # Link all of the scripts into home/bin.
         for script in glob.glob(os.path.abspath('.files/bin') + '/*.py'):
-            if 'py2exe' in script: continue
-            os.system('ln -s %s bin/%s' % (script, os.path.splitext(os.path.basename(script))[0]))
+            if 'py2exe' in script:
+                continue
+            os.system('ln -s %s bin/%s' % (script,
+                                           splitext(basename(script))[0]))
 
         # Link the bash and vim settings.
         os.system('rm -rf .bashrc .profile .vim .vimrc')
@@ -45,4 +50,5 @@ def main():
         os.system('ln -s .files/gitignore .gitignore')
         os.system('ln -s .files/.gitconfig .gitconfig')
 
-if __name__=='__main__': main()
+if __name__ == '__main__':
+    main()
