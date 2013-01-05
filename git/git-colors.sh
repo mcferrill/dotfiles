aee0f8d0988d7a09fca43c47adb964026bd57672
Micah Ferrill <mcferrill@gmail.com>
2013-01-04 18:37:29 -0600

    Moved git to end of bash prompt.

--- git/git-colors.sh
+++ git/git-colors.sh
@@ -97,7 +97,7 @@ if [ $? -eq 0 ]; then \
   echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
   if [ "$?" -eq "0" ]; then \
     # @4 - Clean repository - nothing to commit
-    echo "'$Green'"$(__git_ps1 " (%s)"); \
+    echo "'$Green'"$(__git_ps1 " (%s)"'$Color_Off'); \
   else \
     # @5 - Changes to working tree
     echo "'$IRed'"$(__git_ps1 " {%s}"'$Color_Off'); \
