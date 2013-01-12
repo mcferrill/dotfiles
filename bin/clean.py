be000dc8bbcb63d7a8f9b2c91e0e7f7310a1bcef
Micah Ferrill <mcferrill@gmail.com>
2013-01-11 20:49:23 -0600

    Rewrote clean script.

--- bin/clean.py
+++ bin/clean.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-import os, sys
+"""Remove common cached files (.pyc, <name>~, #<name>#, etc.)."""
+
+import os
+import sys
+
 
 def walk(dirname=os.curdir):
     for basename in os.listdir(dirname):
@@ -8,8 +12,14 @@ def walk(dirname=os.curdir):
         if os.path.isdir(pathname) and '-r' in sys.argv:
             walk(pathname)
         else:
-            if basename.endswith('~') or (basename.startswith('#') and basename.endswith('#')) or basename.endswith('.pyc'):
+            if basename.endswith('~'):
+                os.remove(pathname)
+            elif basename.endswith('~'):
+                os.remove(pathname)
+            elif basename.startswith('#') and basename.endswith('#'):
+                os.remove(pathname)
+            elif basename.endswith('.pyc'):
                 os.remove(pathname)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     walk()
