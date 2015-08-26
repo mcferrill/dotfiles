385e5474455531d4e1de9067e10f35e57245e143
Micah Ferrill <mcferrill@gmail.com>
2015-08-26 12:17:29 -0500

    Tweaked prompt to match user@host pwd_basename layout.

--- git/git-colors.sh
+++ git/git-colors.sh
@@ -92,20 +92,20 @@ User="\u"
 # This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
 # I tweaked it to work on UBUNTU 11.04 & 11.10 plus made it mo' better
 
-export PS1=$White$User$IWhite@$Yellow$Host$Color_Off' '$White$PathFull$Color_Off'$(git branch &>/dev/null;\
-if [ $? -eq 0 ]; then \
-  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
-  if [ "$?" -eq "0" ]; then \
-    # @4 - Clean repository - nothing to commit
-    echo "'$Green'"$(__git_ps1 " (%s)"'$Color_Off'); \
-  else \
-    # @5 - Changes to working tree
-    echo "'$IRed'"$(__git_ps1 " (%s)"'$Color_Off'); \
-  fi) \$ "; \
-else \
-  # @2 - Prompt when not in GIT repo
-  echo "\$ "; \
-fi)'
+#export PS1=$White$User$IWhite@$Yellow$Host$Color_Off' '$White$PathFull$Color_Off'$(git branch &>/dev/null;\
+#if [ $? -eq 0 ]; then \
+#  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+#  if [ "$?" -eq "0" ]; then \
+#    # @4 - Clean repository - nothing to commit
+#    echo "'$Green'"$(__git_ps1 " (%s)"'$Color_Off'); \
+#  else \
+#    # @5 - Changes to working tree
+#    echo "'$IRed'"$(__git_ps1 " (%s)"'$Color_Off'); \
+#  fi) \$ "; \
+#else \
+#  # @2 - Prompt when not in GIT repo
+#  echo "\$ "; \
+#fi)'
 
 #export PS1=$White$User$IWhite@$Yellow$Host$Color_Off' '$IBlack$PathFull$Color_Off' $ ' 
 #export PROMPT_COMMAND='__git_ps1 "$White\u$IWhite@$Yellow\h $IBlack\W$Color_Off" "\\\$ "' 
