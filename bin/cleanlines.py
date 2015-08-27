8beb540ef4eb8e253d2b7e5ed5dc9b77cca091e1
Micah Ferrill <mcferrill@gmail.com>
2015-08-27 10:51:40 -0500

    Updated cleanlines script to use 'with' statements.

--- bin/cleanlines.py
+++ bin/cleanlines.py
@@ -7,10 +7,9 @@ import sys
 
 if __name__ == '__main__':
     for filename in glob.glob(sys.argv[1]):
-        f = open(filename, 'rb')
-        src = f.read()
-        f.close()
-        f = open(filename, 'wb')
-        for line in src.splitlines():
-            f.write(line.rstrip() + '\n')
-        f.close()
+        with open(filename, 'rb') as f:
+            src = f.read()
+        # @todo: replace line endings with unix-style?
+        with open(filename, 'wb') as f:
+            for line in src.splitlines():
+                f.write(line.rstrip() + '\n')
