e8438266b6db9a614d18264c4447f27e1189a6d2
Micah Ferrill <mcferrill@gmail.com>
2019-03-25 09:50:57 -0500

    Updated cleanlines to be python3 friendly.

--- bin/cleanlines.py
+++ bin/cleanlines.py
@@ -7,9 +7,10 @@ import sys
 
 if __name__ == '__main__':
     for filename in glob.glob(sys.argv[1]):
+        src = b''
         with open(filename, 'rb') as f:
             src = f.read()
         # @todo: replace line endings with unix-style?
         with open(filename, 'wb') as f:
             for line in src.splitlines():
-                f.write(line.rstrip() + '\n')
+                f.write(line.rstrip() + b'\n')
