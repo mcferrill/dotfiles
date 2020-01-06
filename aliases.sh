5895dfc4e514abdd6a49f11658acc965836eeac3
Micah Ferrill <mcferrill@gmail.com>
2020-01-06 11:30:22 -0600

    Refactoring multiple config files to cut file sizes and improve readability.

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+
+alias mv="mv -i"
+alias rm="rm -i"
+alias cp="cp -i"
+alias ls="ls --color"
+alias ll="ls -l"
+alias la="ls -A"
+alias cls="clear"
+alias tree="tree -C"
+alias free="free -th"
+alias grep="grep --color -n"
+alias screen="screen -s bash" # Make sure bashrc gets executed in cygwin.
+
+export PATH=$HOME/bin:$PATH
+export VISUAL=vim
