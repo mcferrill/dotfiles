1504041dbe4440ec9643c06f8a5452c2c6855aa5
Micah Ferrill <mcferrill@gmail.com>
2021-07-31 00:00:46 -0500

    Added powershell profile template.
    Former-commit-id: 888218a3993de971c26f06fde3c107be96d005fb

---
+++ misc/Powershell.ps1
@@ -0,0 +1,10 @@
+# Install
+# Install-Module PSReadLine -AllowPrerelease -Force
+
+# $profile
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+}
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
