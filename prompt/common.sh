9379cba223742aa7c9064e7d03a6e12a34e92ce1
Micah Ferrill <mcferrill@gmail.com>
2024-02-23 07:41:50 -0600

    Disabling virtualenvwrapper by default for now.

--- prompt/common.sh
+++ prompt/common.sh
@@ -11,12 +11,13 @@ fi
 WORKON_HOME=$HOME/.envs
 PIP_VIRTUALENV_BASE=$WORKON_HOME
 PIP_RESPECT_VIRTUALENV=true
-export VIRTUALENVWRAPPER_PYTHON=$(which python3)
-if [ "$VIRTUAL_ENV" ]; then
-    source $VIRTUAL_ENV/bin/activate;
-elif [ "$(which virtualenvwrapper.sh)" ]; then
-    source "$(which virtualenvwrapper.sh)"
-fi
+# Disabled for now, manually enable via sys.sh if needed.
+# export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+# if [ "$VIRTUAL_ENV" ]; then
+#     source $VIRTUAL_ENV/bin/activate;
+# elif [ "$(which virtualenvwrapper.sh)" ]; then
+#     source "$(which virtualenvwrapper.sh)"
+# fi
 
 # fnm (rust-based nvm/n alternative)
 if command -v fnm &> /dev/null
