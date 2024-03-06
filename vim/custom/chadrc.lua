1e3654de3385c407035efa6f30ecb23407f6ee91
Micah Ferrill <mcferrill@gmail.com>
2024-03-06 06:49:28 -0600

    Set theme to wombat.

--- vim/custom/chadrc.lua
+++ vim/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "github_light",
-  theme_toggle = { "github_light", "github_light" },
+  theme = "wombat",
+  theme_toggle = { "wombat", "wombat" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
