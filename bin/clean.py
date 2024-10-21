91cd1ff5ff1141b70fb6c6a74291cf31bb965994
Micah Ferrill <mcferrill@pm.me>
2024-10-21 07:09:01 -0500

    Formatting.

--- bin/clean.py
+++ bin/clean.py
@@ -6,22 +6,22 @@ import os
 import shutil
 
 FILE_ENDINGS = (
-    '~',
-    '#',
-    '.pyc',
-    '.DS_Store',
-    '._.DS_Store',
+    "~",
+    "#",
+    ".pyc",
+    ".DS_Store",
+    "._.DS_Store",
 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for dirpath, dirnames, filenames in os.walk(os.curdir):
         for dirname in dirnames:
-            if dirname.endswith('__pycache__'):
+            if dirname.endswith("__pycache__"):
                 shutil.rmtree(os.path.join(dirpath, dirname), True)
-        
+
         for name in filenames:
             for ending in FILE_ENDINGS:
                 if name.endswith(ending):
-                    os.remove(os.path.join(dirname, name))
+                    os.remove(os.path.join(dirpath, name))
                     break
--- bin/cleanlines.py
+++ bin/cleanlines.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-"""Strips excess whitespace off the end of lines of code."""
+"""Strips excess whitespace off the end of lines of code and replaces lineendings with "\n"."""
 
 import glob
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for filename in glob.glob(sys.argv[1]):
-        src = b''
-        with open(filename, 'rb') as f:
+        src = b""
+        with open(filename, "rb") as f:
             src = f.read()
-        with open(filename, 'wb') as f:
+        with open(filename, "wb") as f:
             for line in src.splitlines():
-                f.write(line.rstrip() + b'\n')
+                f.write(line.rstrip() + b"\n")
