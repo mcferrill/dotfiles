8d65acc184356c71e6a68cc6243d73dff7b2a872
Micah Ferrill <mcferrill@pm.me>
2024-11-11 07:53:13 -0600

    Implemented dark mode toggling for wezterm & nvim.

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,7 +1,24 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Kanagawa (Gogh)"
+function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+    if appearance:find("Dark") then
+        return "Kanagawa (Gogh)"
+    else
+        return "Builtin Solarized Light"
+    end
+end
+
+-- config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = scheme_for_appearance(get_appearance())
+
 config.automatically_reload_config = true
 config.font = wezterm.font_with_fallback({
     { family = "FiraCode Nerd Font", weight = "DemiBold" },
@@ -9,6 +26,7 @@ config.font = wezterm.font_with_fallback({
 })
 
 config.enable_tab_bar = false
+config.window_background_opacity = 0.90
 
 -- Use smaller font size on windows
 if package.config:sub(1, 1) == "/" then
