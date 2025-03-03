6cd2d50de0b6240fa5a36cdb9c0871d58e59bbdf
Micah Ferrill <mcferrill@pm.me>
2025-03-03 09:23:13 -0600

    Playing with markdown config.

--- config/nvim.lua
+++ config/nvim.lua
@@ -88,30 +88,39 @@ require("lazy").setup({
     { "xiyaowong/transparent.nvim", lazy = false },
     { "mrjones2014/smart-splits.nvim", lazy = false },
     {
-        "epwalsh/obsidian.nvim",
-        version = "*",
-        lazy = true,
-        ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-        --   -- refer to `:h file-pattern` for more examples
-        --   "BufReadPre path/to/my-vault/*.md",
-        --   "BufNewFile path/to/my-vault/*.md",
-        -- },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        opts = {
-            workspaces = {
-                {
-                    name = "Notes",
-                    path = "~/vault",
-                },
-            },
-        },
+        "MeanderingProgrammer/render-markdown.nvim",
+        -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
     },
+    -- {
+    --     "epwalsh/obsidian.nvim",
+    --     version = "*",
+    --     lazy = true,
+    --     ft = "markdown",
+    --     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    --     -- event = {
+    --     --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --     --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --     --   -- refer to `:h file-pattern` for more examples
+    --     --   "BufReadPre path/to/my-vault/*.md",
+    --     --   "BufNewFile path/to/my-vault/*.md",
+    --     -- },
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --     },
+    --     opts = {
+    --         workspaces = {
+    --             {
+    --                 name = "Notes",
+    --                 path = "~/vault",
+    --             },
+    --         },
+    --     },
+    -- },
     {
         "nvim-neo-tree/neo-tree.nvim",
         dependencies = {
