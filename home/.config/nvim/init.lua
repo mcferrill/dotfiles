5de3aa31c34ed2497d0f82a3d483a621e7c889db
Micah Ferrill <mcferrill@pm.me>
2026-09-17 11:57:35 -0500

    Added markdown-preview to vim config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -663,6 +663,23 @@ require("lazy").setup({
         end,
     },
 
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        keys = {
+            {
+                "<leader>mp",
+                "<cmd>MarkdownPreviewToggle<cr>",
+                ft = "markdown",
+                desc = "[M]arkdown [P]review",
+            },
+        },
+    },
+
     -- Highlight todo, notes, etc in comments
     {
         "folke/todo-comments.nvim",
